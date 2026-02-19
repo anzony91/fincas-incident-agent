fc4edf8b3a106bee0b78e1e9f0e76a2c6a3d1208
Antonio Perez <antcperez@cosentino.com>
2026-02-19 13:07:50 +0100

    fix: correct down_revision reference to 001_initial

--- alembic/versions/002_add_channel.py
+++ alembic/versions/002_add_channel.py
@@ -1,7 +1,7 @@
 """add channel and reporter_phone to tickets
 
 Revision ID: 002
-Revises: 001
+Revises: 001_initial
 Create Date: 2026-02-19
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '002'
-down_revision: Union[str, None] = '001'
+down_revision: Union[str, None] = '001_initial'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
